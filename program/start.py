e7171181cc260197763f8797bbc38a429c4cfd14
LOGESH <95044430+LOGI-LAP@users.noreply.github.com>
2021-12-24 19:56:19 +0530

    Update start.py

--- program/start.py
+++ program/start.py
@@ -58,6 +58,8 @@ async def start_(client: Client, message: Message):
 
 💡 **Find out all the Bot's commands and how they work by clicking on the » 📚 Commands button!**
 
+🛠 [🕊.⋆𝙆𝙄𝙉𝙂༒۝꧂](https://t.me/cl_me_logesh) **if you have any problems contact 💚**
+
 ❔ **To know how to use this bot, please click on the » ❓ Basic Guide button!**""",
         reply_markup=InlineKeyboardMarkup(
             [
