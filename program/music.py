7cf10908f8d954cdae2ed8e04863b34876c43255
LOGESH <95044430+LOGI-LAP@users.noreply.github.com>
2021-12-26 10:09:19 +0530

    Update music.py

--- program/music.py
+++ program/music.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 By Veez Music-Project
+# Copyright (C) 2021 By logi music-player
 # Commit Start Date 20/10/2021
 # Finished On 28/10/2021
 
@@ -39,7 +39,7 @@ async def ytdl(format: str, link: str):
     return 0, stderr
 
 
-@Client.on_message(command(["mplay", f"mplay@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command(["play", f"mplay@{BOT_USERNAME}"]) & other_filters)
 async def play(c: Client, m: Message):
     await m.delete()
     replied = m.reply_to_message
